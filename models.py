b56a9776cbbc0aac3f617ac6d09da345be0e12af
daniel <sirkaranja12@gmail.com>
2023-06-28 18:31:18 +0300

    models initialization

---
+++ models.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db= SQLAlchemy()
+
+class Director(db.Model):
+    __tablename__ = 'directors'
+
+    id= db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String, unique=True)
+    department= db.relationship('Department', backref='directors')
+
+    def __repr__(self):
+        return f'<Department Director {self.name}'
+    
+class Department():
+    __tablename__='departments'
+
+    id= db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String)
+    specialization=db.Column(db.String)
+
+    director_id = db.Column(db.Integer, db.ForeignKey('directors.id'))
+
+    def __repr__(self):
+        return f'<Department{self.name}, {self.specialization}'
