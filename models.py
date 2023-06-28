65c5521de0a3561e71ad7e4a090cd38e0baf7c96
daniel <sirkaranja12@gmail.com>
2023-06-28 18:47:50 +0300

    replaced the attributes initialization

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ class Director(db.Model):
     def __repr__(self):
         return f'<Department Director {self.name}'
     
-class Department():
+class Department(db.models):
     __tablename__='departments'
 
     id= db.Column(db.Integer, primary_key=True)
