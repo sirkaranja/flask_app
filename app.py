0e28fa87ccd86dff697efea03db226d37ec0cda6
daniel <sirkaranja12@gmail.com>
2023-06-28 19:45:15 +0300

    route to view all direcotrs

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
-from models import db
+from models import Director, db
 
 app= Flask(__name__)
 
@@ -16,10 +16,29 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 
-
+#view the deparments and directors
 @app.route('/')
 def index():
-    return '<h2 style="centre">Welcome to my first flask app</h2>'
+    response= make_response(
+        '<h1>Hi welcome to KQ Company</h1>',
+        200
+    )
+    return response
+
+@app.route('/directors')
+def all_directors():
+    directors = Director.query.all()
+
+    response_body = '<h1>Directors List</h1>'
+    for director in directors:
+        response_body += f'<h2>{director.name}</h2>'
+    
+    response = make_response(response_body, 200)
+    return response
+    
+# @app.route('/')
+# def index():
+#     return '<h2 style="centre">Welcome to my first flask app</h2>'
 
 # @app.route('/<string:username>')
 # def user(username):
@@ -28,4 +47,4 @@ def index():
 
 
 if __name__ =='__main__':
-    app.run(port=5504)
+    app.run(port=5506)
