d0227a5da50b9f752c828d9605e32f93b61be9be
daniel <sirkaranja12@gmail.com>
2023-06-28 19:21:53 +0300

    seeding data into db

--- seed.py
+++ seed.py
@@ -4,13 +4,29 @@ from faker import Faker
 from app import app
 from models import db, Director, Department
 
-db.init_app(app)
+
 
 fake= Faker()
 
 with app.app_context():
     Director.query.delete()
     Department.query.delete()
+
+    directors=[]
+    for n in range(15):
+        director= Director(name=fake.name())
+        directors.append(director)
+
+    db.session.add_all(directors)
+
+    departments=[]
+    depart_offices=['Accounting', 'Software Developer', 'Sports', 'Health','Finance','Human Resource']
+    for n in range(30):
+        depart= Department(name=fake.name(), specialization=rc(depart_offices))
+        departments.append(depart)
+
+    db.session.add_all(departments)
+    db.session.commit()
 
     
 
